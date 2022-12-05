a1d58601e937651ace809edda389efcf07b7b345
puzz <taste@inbox.ru>
2022-12-05 21:05:53 +0300

    make last home work. Thanks a lot for you job

---
+++ task0/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 64: Задайте значение N. 
+Напишите программу, которая выведет 
+все натуральные числа в промежутке от N до 1.
+ Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+int getUserInt(string message)
+{
+    Console.ForegroundColor = ConsoleColor.Gray;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int UserInt = Convert.ToInt32(Console.ReadLine());
+    return UserInt;
+}
+int userStartIndex = getUserInt("Put max element");
+printRange(userStartIndex);
+
+void printRange(int index)
+{
+    if (index !=0)
+    {
+        Console.Write(index + ", ");// если наоборот очередь
+        printRange(index-1);  // поменять местаии
+    }
+        
+}
---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму
+ натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+void resultNumber(int m, int n, int result)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"(Sum of numbers {result})");
+        return;
+    }
+    result = result + (m++);
+    resultNumber(m,n,result);
+}
+
+Console.Write(" Put First Number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write(" Put Second Number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+resultNumber (m,n,0);
